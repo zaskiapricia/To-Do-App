1c2f7ea7eb1d6d83c8643093b6f125073169938a
Zaskia Apricia Putri Siregar <zaskiaapricia7@gmail.com>
2026-04-19 04:25:15 +0700

    edit index.html

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
